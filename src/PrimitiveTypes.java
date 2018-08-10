33faa6ef61ad08beba3075ccb96f6ae73507cd3a
sahibdes <sahibdeeps@gmail.com>
2018-08-09 20:02:31 -0700

    codes

--- src/PrimitiveTypes.java
+++ src/PrimitiveTypes.java
@@ -1,3 +1,7 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * Questions related to bit manipulation and primitive types.
  * Tricky is the bit manipulation
@@ -93,7 +97,126 @@ public class PrimitiveTypes {
 		return (n1 & (n2-1));
 	}
 	
+	/**
+	 * Swap bits in a 64 bit word on ith and jth position.
+	 * Ex 110000,2,5=100100
+	 * Hint: 1. Check if both the bits at ith and jth are same
+	 * 		 2. Use bitmask to flip bits if not same. (Use left signed operation to move position i and j)
+	 * @param num
+	 * @param i
+	 * @param j
+	 * @return
+	 */
+	public static long swapBits(long num, int i, int j) {
+		if(i>=j)
+			return -1L;
+		
+		if(((num>>>i) &1) == ((num >>> j) &1))
+			return num;
+		//move 1 to ith and jth position and then or. This will give the bitmask to be xored with.
+		
+		long bitMask=1L << i | 1L << j;
+		return num^bitMask;
+	}
 	
+	
+	public static double sqrt(int N){
+		double X=1;
+		for(int i=0;i<=N/2;i++){
+			double fX=Math.pow(X, 2)-N;
+			double f_X=2*X;
+			X=X-(fX/f_X);
+		}
+		return X;
+	}
+	
+	/*
+	 * reverse integer and check for reverse integer overflow
+	 */
+public static int reverseInt(int x){
+
+	long result=0;
+	int xRemaining=Math.abs(x);
+	while (xRemaining>0){
+		//1132 2 + x%10=113 x/10 3 x%10=11	
+		result=result*10+xRemaining%10;
+		if(result>Integer.MAX_VALUE)
+			return 0;
+		xRemaining=xRemaining/10;
+	}
+	return x>=0?(int)result:-(int)(result);
+}
+
+public static String longestPrefix(String[] strs){
+	String common="";
+	if(strs.length<2)
+		return "";
+	
+	
+	String firstElem=strs[0];
+	
+	for(int c=0;c<firstElem.length();c++)
+	{
+		int count=0;
+		for (int i=1;i<strs.length;i++){
+			
+			if(c>=strs[i].length())
+				break;
+			
+			if(strs[i].charAt(c)==firstElem.charAt(c)){
+				count++;
+			if(count==strs.length-1)
+				common+=firstElem.charAt(c);
+			}
+			
+		}
+		if(count!=strs.length-1)
+			break;
+	}
+	return common;
+}
+
+/**
+ * check for valid parentheses
+ * @param s
+ * @return
+ */
+public static boolean isValid(String s) {
+ 	boolean valid=false;
+    
+if(s=="")
+  return true;
+    
+if(s.length()%2!=0)
+	return valid;
+
+Stack<Character> st=new Stack<>();
+
+for(int i=0;i<s.length();i++){
+	char out=s.charAt(i);	
+	if(st.empty())
+	st.push(s.charAt(i));
+	else{
+		char in=st.peek();
+		if((in=='(' && out==')') || (in=='{' && out=='}') || (in=='[' && out==']')){
+			st.pop();
+			valid =true;
+	}
+			else{
+				st.push(s.charAt(i));
+				valid =false;
+			}
+		}
+	}
+
+if(!st.isEmpty())
+	return false;
+
+return valid;
+}
+
+
+
 	public static void main(String[] args) {
 		// number of 1s
 		System.out.println(countBits(4));
@@ -103,6 +226,31 @@ public class PrimitiveTypes {
 		System.out.println(checkParity(4));//1
 		System.out.println(checkParity(6));//0
 		System.out.println(calcMod(77,64));
-	}
+		System.out.println(swapBits(4, 0, 2));
+		System.out.println(sqrt(5863332));
+
+		System.out.println(reverseInt(1534236469));
+		
+		String[] strs={"flower","flow","flight"};
+		System.out.println(longestPrefix(strs));
+		
+
+		String[] strs1={"doger","dog","flight"};
+		System.out.println(longestPrefix(strs1));
+		
+		
+		String[] strs2={"flower","flow","folwer"};
+		System.out.println(longestPrefix(strs2));
+		
+		String[] strs3={"a"};
+		System.out.println(longestPrefix(strs2));
+		
+		System.out.println(isValid("{[]}"));
+		System.out.println(isValid("{}[]()"));
+		System.out.println(isValid("{[]()"));
+		System.out.println(isValid(""));
+		System.out.println(isValid("(()("));
+		System.out.println(isValid("}}(]}}){)(])](}{{}[]"));
+ 	}
 
 }
